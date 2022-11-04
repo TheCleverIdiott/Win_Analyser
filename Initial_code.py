1b9e71a8a5437a4caa4dddced4d4bfcd4541022f
Anoushka Sen <114612946+senanoushka@users.noreply.github.com>
2022-11-04 22:30:21 +0530

    Update Initial_code.py

--- Initial_code.py
+++ Initial_code.py
@@ -31,7 +31,7 @@ matches["round"].value_counts()
 
 
 matches.dtypes
-#shows the data types, ML algos can only work with numeric data so any data we feed into the model must be numneric... so object must be converted
+#shows the data types, ML algos can only work with numeric data so any data we feed into the model must be numeric... so object must be converted
 
 
 del matches["comp"]
@@ -39,7 +39,7 @@ del matches["notes"]
 #we delete unnecessary data ofr this
 
 matches["date"] = pd.to_datetime(matches["date"])
-# crdeating an column with datetim,e and opverwriting the existing column with date time
+# crdeating an column with datetime and operwriting the existing column with date time
 
 
 matches["target"] = (matches["result"] == "W").astype("int")
@@ -47,10 +47,10 @@ matches["target"] = (matches["result"] == "W").astype("int")
 matches
 
 
-#creating predictors to build basic ML model that we can add forther complexity to later
+#creating predictors to build basic ML model that we can add further complexity to later
 
 matches["venue_code"] = matches["venue"].astype("category").cat.codes
-#converting from string to categories and then converting those categoties to numbers
+#converting venue column from string to categorical data type and then converting those categories to integers 0 and 1
 
 matches["opp_code"] = matches["opponent"].astype("category").cat.codes
 matches["hour"] = matches["time"].str.replace(":.+", "", regex=True).astype("int")
@@ -90,14 +90,56 @@ preds = rf.predict(test[predictors])
 # Now we need to figure out a way to determine the accuaracy of the model.
 # Important choice, so we'rte gonna try a couple of different metrics and see which one makews more sense.
 from sklearn.metrics import accuracy_score
-# accuracy s
-
-acc= accuracy_score(test["target"], preds)
-acc
-combined= pd.DataFrame(dict(actual=test["target"],prediction=preds))
-pd.crosstab(index=combined["actual"], columns=combined["prediction"])
-from sklearn.metrics import precision_score
 precision_score(test["target"], preds)
+
+grouped_matches = matches.groupby("team")
+group = grouped_matches.get_group("Manchester City").sort_values("date")
+
+def rolling_averages(group, cols, new_cols):
+    group = group.sort_values("date")
+    rolling_stats = group[cols].rolling(3, closed='left').mean()
+    group[new_cols] = rolling_stats
+    group = group.dropna(subset=new_cols)
+    return group
+  
+cols = ["gf", "ga", "sh", "sot", "dist", "fk", "pk", "pkatt"]
+new_cols = [f"{c}_rolling" for c in cols]
+
+rolling_averages(group, cols, new_cols)
+
+matches_rolling = matches.groupby("team").apply(lambda x: rolling_averages(x, cols, new_cols))
+matches_rolling
+
+matches_rolling = matches_rolling.droplevel('team')
+matches_rolling
+
+matches_rolling.index = range(matches_rolling.shape[0])
+def make_predictions(data, predictors):
+    train = data[data["date"] < '2022-01-01']
+    test = data[data["date"] > '2022-01-01']
+    rf.fit(train[predictors], train["target"])
+    preds = rf.predict(test[predictors])
+    combined = pd.DataFrame(dict(actual=test["target"], predicted=preds), index=test.index)
+    error = precision_score(test["target"], preds)
+    return combined, error
+combined, error = make_predictions(matches_rolling, predictors + new_cols)
+
+combined = combined.merge(matches_rolling[["date", "team", "opponent", "result"]], left_index=True, right_index=True)
+combined.head(10)
+
+class MissingDict(dict):
+    __missing__ = lambda self, key: key
+
+map_values = {"Brighton and Hove Albion": "Brighton", "Manchester United": "Manchester Utd", "Newcastle United": "Newcastle Utd", "Tottenham Hotspur": "Tottenham", "West Ham United": "West Ham", "Wolverhampton Wanderers": "Wolves"} 
+mapping = MissingDict(**map_values)
+combined["new_team"] = combined["team"].map(mapping)
+merged = combined.merge(combined, left_on=["date", "new_team"], right_on=["date", "opponent"])
+merged
+
+merged[(merged["predicted_x"] == 1) & (merged["predicted_y"] ==0)]["actual_x"].value_counts()
+
+
+
 
 
 
